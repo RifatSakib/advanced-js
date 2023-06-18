8bac62b7b76030ab24e98b55ef214931f302a98a
Rifat Sakib <sakibrifat353@gmail.com>
2023-06-19 00:13:55 +0600

    clear nullundefined

---
+++ null-vs-undefine.js
@@ -0,0 +1,26 @@
+//undefine
+let pakhi;
+console.log(pakhi);
+
+function add (num1,num2){
+
+
+    console.log(num1 + num2);
+    // return
+
+}
+
+const result= add(13,82);
+console.log(result);
+
+const premik= {name:"Smart dude", phone: '01235768'};
+console.log(premik.gf);
+
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,5,78];
+console.log(ages[10]);
+
+//null
