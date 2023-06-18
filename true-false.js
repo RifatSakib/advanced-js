83ac61786c96c80686de4e1faea37fd8f49fad4f
Rifat Sakib <sakibrifat353@gmail.com>
2023-06-18 14:36:23 +0600

    first commit

---
+++ true-false.js
@@ -0,0 +1,27 @@
+
+// all false
+//false
+//null
+//undefine
+//0
+//""
+//NaN  -not a number 
+
+//all true
+//"false"
+// '0'
+// ' '
+//[]
+// let name = {} -object
+
+const name = {}
+
+if (name){
+
+    console.log( "condition is true");
+
+}
+
+else{
+    console.log("condition is false")
+}
